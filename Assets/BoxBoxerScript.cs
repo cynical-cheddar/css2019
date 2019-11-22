13b99d34132ae3d011e81f4323cfb15908f4ac65
Unknown <matthew.crees@ntlworld.com>
2019-11-22 21:35:46 +0000

    Boxes spawn and conveyors belt

---
+++ Assets/BoxBoxerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxBoxerScript : MonoBehaviour
+{
+    public GameObject box;
+    public float spawnrate;
+    private float cooldown;
+
+    void Start()
+    {
+        cooldown = spawnrate;
+    }
+
+    void Update()
+    {
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0)
+        {
+            Instantiate(box, transform.position, Quaternion.identity);
+            cooldown = spawnrate;
+        }
+
+    }
+}

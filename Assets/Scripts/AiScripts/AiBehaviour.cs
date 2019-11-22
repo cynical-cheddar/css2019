61204a2c1a8eb7517a0b9bbf625223b3baea7fe4
CynicalCheddar <xz18677@bristol.ac.uk>
2019-11-22 13:49:22 +0000

    Ai Vision Implemented

---
+++ Assets/Scripts/AiScripts/AiBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AiBehaviour : MonoBehaviour {
+
+	public float cooldown;
+	public float fireRate = 1f;
+	float prevSpeed;
+	public virtual void setTarget (Transform target){
+	}
+   
+	public virtual void unclaimCover (){
+	}
+	public virtual bool setHasTarget (bool hasTarget){
+		return false;
+	}
+	public virtual bool getHasTarget (){
+		return false;
+	}
+	public void applyPain(){
+
+	}
+	void restoreSpeed(){
+
+	}
+    public virtual void removeTarget()
+    {
+
+    }
+
+}

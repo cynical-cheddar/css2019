101b9b8cc149137e42790679065fad46513587d3
CynicalCheddar <xz18677@bristol.ac.uk>
2019-11-22 14:06:47 +0000

    Created reactor script
    Method is called in 'AiCameraReactor' script when the player has been seen

---
+++ Assets/AiCameraReactor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiCameraReactor : AiBehaviour
+{
+
+    public Transform detectedPlayer;
+    public AudioClip detectedSound;
+    public AudioSource detectorAudiosource;
+    // This is called when a detector has seen you.
+    // How should we react?
+	public override void setTarget (Transform target){
+        detectedPlayer = target;
+        if(detectorAudiosource != null && detectedSound != null){
+            detectorAudiosource.clip = detectedSound;
+            detectorAudiosource.Play();
+        }
+
+	}
+   
+
+}

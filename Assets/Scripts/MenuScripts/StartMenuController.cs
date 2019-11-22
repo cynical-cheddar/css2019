40d834eb3ff7dea64d29d5c17d5d0ca53dbb8ab1
viper12590 <viper1259@gmail.com>
2019-11-22 23:41:26 +0000

    main menu

---
+++ Assets/Scripts/MenuScripts/StartMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public string firstScene;
+
+   public void PlayGame()
+    {
+        SceneManager.LoadScene(firstScene);
+    }
+
+    public void Quitgame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
